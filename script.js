3edcfd0efb0977ecd1b8c95491ff7728baf472f4
unknown <williamV1905@mhbx.org>
2021-08-09 15:13:21 -0400

    e

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let url =''
+let url ='https://animechan.vercel.app/api/random'
 
 
 fetch(url)
@@ -6,5 +6,26 @@ fetch(url)
     return response.json();
 })
 .then(function(myJson){
+    let animeName=myJson.anime;
+    let myVar =document.createElement('p');
+    myVar.innerHTML=animeName;
+    let body=document.querySelector('body');
+    body.appendChild(myVar);
+    
+    let animeCharacter=myJson.character;
+    let secondP=document.createElement('p');
+    secondP.innerHTML=animeCharacter;
+  
+    body.appendChild(secondP);
+    
+    let quote=myJson.quote;
+    let thirdP=document.createElement('p');
+    thirdP.innerHTML=quote;
+    
    
+    body.appendChild(thirdP);
 });
+
+<div id="textSize">
+    
+</div>
